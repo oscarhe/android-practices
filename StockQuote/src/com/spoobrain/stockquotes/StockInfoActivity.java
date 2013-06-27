6f5f4a84a4dc18c5d15e2e4fe33948b9b1a5474f
Oscar He <oscarhe91@gmail.com>
2013-06-27 18:41:38 -0400

    Added xml pull parser to parse YQL data. Still does not work on android emulator for some reason even though it does not display any errors

--- StockQuote/src/com/spoobrain/stockquotes/StockInfoActivity.java
+++ StockQuote/src/com/spoobrain/stockquotes/StockInfoActivity.java
@@ -2,8 +2,11 @@ package com.spoobrain.stockquotes;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -11,10 +14,17 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -22,6 +32,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+
 
 public class StockInfoActivity extends Activity {
 	
@@ -58,6 +69,16 @@ public class StockInfoActivity extends Activity {
 	String yahooURLFirst = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22";
 	String yahooURLSecond = "%22)&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
 	
+	String[][] xmlPullParserArray = {{"AverageDailyVolume", "0"}, 
+			{"Change", "0"}, {"DaysLow", "0"}, 
+			{"DaysHigh", "0"}, {"YearLow", "0"}, 
+			{"YearHigh", "0"}, {"MarketCapitalization", "0"}, 
+			{"LastTradePriceOnly", "0"}, {"DaysRange", "0"}, 
+			{"Name", "0"}, {"Symbol", "0"},
+			{"Volume", "0"}, {"StockExchange", "0"}};
+	
+	int parserArrayIncrement = 0;
+	
 	protected void onCreate(Bundle savedInstanceState) { 
 		
 		super.onCreate(savedInstanceState);
@@ -88,7 +109,7 @@ public class StockInfoActivity extends Activity {
 
 		@Override
 		protected String doInBackground(String... args) {
-			
+			/*
 			try {
 				
 				URL url = new URL(args[0]);
@@ -146,25 +167,115 @@ public class StockInfoActivity extends Activity {
 				Log.d(TAG, "SAX Exception", e);
 			}
 			finally { }
+			*/
+			
+			try {
+				
+				XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+				factory.setNamespaceAware(true);
+				XmlPullParser parser = factory.newPullParser();
+				parser.setInput(new InputStreamReader(getUrlData(args[0])));
+				
+				beginDocument(parser, "query");
+				
+				int eventType = parser.getEventType();
+				
+				do {
+					
+					nextElement(parser);
+					
+					parser.next();
+					
+					eventType = parser.getEventType();
+					
+					if(eventType == XmlPullParser.TEXT) {
+						
+						String valueFromXML = parser.getText();
+						
+						xmlPullParserArray[parserArrayIncrement++][1] = valueFromXML;
+						
+					}
+					
+				} while (eventType != XmlPullParser.END_DOCUMENT); 
+				
+			} catch (XmlPullParserException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (URISyntaxException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			finally {}
 			
 			return null;
 			
 		}
 		
-		protected void onPostExecute(String result) {
+		public InputStream getUrlData(String url) throws URISyntaxException, ClientProtocolException, IOException {
 			
-			companyNameTextView.setText(name);
-			yearLowTextView.setText("Year Low: " + yearLow);
-			yearHighTextView.setText("Year High: " + yearHigh);
-			daysLowTextView.setText("Days Low: " + daysLow);
-			daysHighTextView.setText("Days High: " + daysHigh);
-			lastTradePriceOnlyTextView.setText("Last Price: " + lastTradePriceOnly);
-			changeTextView.setText("Change: " + change);
-			daysRangeTextView.setText("Daily Price Range: " + daysRange);
+			DefaultHttpClient client = new DefaultHttpClient();
+			HttpGet method = new HttpGet(new URI(url));
+			
+			HttpResponse res = client.execute(method);
+			return res.getEntity().getContent();
 			
 		}
 		
-		private StockInfo getStockInformation(Element entry) {
+		public final void beginDocument(XmlPullParser parser, String firstElementName) throws XmlPullParserException, IOException {
+			
+			int type;
+			
+			while ((type = parser.next()) != parser.START_TAG && type != parser.END_DOCUMENT) {
+				
+				;
+				
+			}
+			
+			if (type != parser.START_TAG) {
+				
+				throw new XmlPullParserException("No Start Tag Found");
+				
+			}
+			
+			if (!parser.getName().equals(firstElementName)) {
+				
+				throw new XmlPullParserException("Unexpected Start Tag Found: " + parser.getName() + " Expected Start Tag: " + firstElementName);
+				
+			}
+			
+		}
+		
+		public final void nextElement(XmlPullParser parser) throws XmlPullParserException, IOException {
+			
+			int type;
+			
+			while ((type = parser.next()) != parser.START_TAG && type != parser.END_DOCUMENT) {
+				;
+			}
+			
+		}
+		
+		protected void onPostExecute(String result) {
+			
+			companyNameTextView.setText(xmlPullParserArray[9][1]);
+			yearLowTextView.setText(xmlPullParserArray[4][1]);
+			yearHighTextView.setText(xmlPullParserArray[5][1]);
+			daysLowTextView.setText(xmlPullParserArray[2][1]);
+			daysHighTextView.setText(xmlPullParserArray[3][1]);
+			lastTradePriceOnlyTextView.setText(xmlPullParserArray[7][1]);
+			changeTextView.setText(xmlPullParserArray[1][1]);
+			daysRangeTextView.setText(xmlPullParserArray[8][1]);
+			
+		}
+		
+		/*private StockInfo getStockInformation(Element entry) {
 			
 			String stockName = getTextValue(entry, "Name");
 			String stockYearLow = getTextValue(entry, "YearLow");
@@ -200,6 +311,7 @@ public class StockInfoActivity extends Activity {
 			return tagValueToReturn;
 			
 		}
+		*/
 		
 	}
 	
