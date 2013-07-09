5837ab40de2658493c2b27983df65cdccf55b87a
Oscar He <oscarhe91@gmail.com>
2013-07-08 21:53:43 -0400

    First commit: Added skeleton layout

---
+++ StudentGrades/README.txt
@@ -0,0 +1,10 @@
+A simple app for teachers to add classes and students.
+Possible features: 
+    grade tracking
+    grade calculation (average, median, mode, etc)
+    grade comparison
+    adding curve
+    attendance tracking
+    log of student behaviors
+
+Point of this project is to help me to become even more familiar with implementing events, adding layouts within layouts, using sqlite and more. Also because the gf is striving to be a teacher...
---
+++ StudentGrades/src/com/spoobrain/studentgrades/DBTools.java
@@ -0,0 +1,28 @@
+package com.spoobrain.studentgrades;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBTools extends SQLiteOpenHelper {
+
+	public DBTools(Context applicationContext) {
+		
+		super(applicationContext, "studentGrades.db", null, 1);
+		
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
