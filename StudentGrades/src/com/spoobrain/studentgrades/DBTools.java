ba19c50fe66a7e33e566361201ebf868be1bcd91
Oscar He <oscarhe91@gmail.com>
2013-07-10 22:27:34 -0400

    Added method to create the database schema when opening the app for the first time.

--- StudentGrades/src/com/spoobrain/studentgrades/DBTools.java
+++ StudentGrades/src/com/spoobrain/studentgrades/DBTools.java
@@ -15,13 +15,55 @@ public class DBTools extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		// TODO Auto-generated method stub
+
+		String createClassTable = "CREATE TABLE classes (" +
+								  "classId INTEGER PRIMARY KEY, " +
+								  "className TEXT, " +
+								  "classType TEXT, " +
+								  "classNumber INTEGER, " +
+								  "classSection TEXT, " +
+								  "classYear INTEGER)";
+		
+		db.execSQL(createClassTable);
+		
+		String createStudentTable = "CREATE TABLE students (" +
+								    "studentId INTEGER PRIMARY KEY, " +
+								    "studentName TEXT, " +
+								    "classId INTEGER, " +
+								    "FOREIGN KEY (classId) " +
+								    "REFERENCES classes(classId) " +
+								    "ON DELETE CASCADE)";
+		
+		db.execSQL(createStudentTable);
+		
+		String createStudentInfoTable = "CREATE TABLE student_info (" +
+										"studentId INTEGER PRIMARY KEY, " +
+										"studentDOB DATE, " +
+										"studentGPA REAL, " +
+										"FOREIGN KEY (studentId) " +
+										"REFERENCES students(studentId) " +
+										"ON DELETE CASCADE)";
+		
+		db.execSQL(createStudentInfoTable);
 		
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		// TODO Auto-generated method stub
+
+		String query = "DROP TABLE IF EXISTS classes";
+		
+		db.execSQL(query);
+		
+		query = "DROP TABLE IF EXISTS students";
+		
+		db.execSQL(query);
+		
+		query = "DROP TABLE IF EXISTS student_info";
+		
+		db.execSQL(query);
+		
+		onCreate(db);
 		
 	}
 
