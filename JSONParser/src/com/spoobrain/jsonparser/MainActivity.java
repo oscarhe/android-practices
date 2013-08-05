2c05db812dc67c43af7cbbe96a3b22e226ead869
Oscar He <oscarhe91@gmail.com>
2013-08-05 14:07:56 -0400

    Added simple layout.

--- JSONParser/src/com/spoobrain/jsonparser/MainActivity.java
+++ JSONParser/src/com/spoobrain/jsonparser/MainActivity.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	static String yahooStockURL = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22GOOG%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=cbfunc";
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
