d0e15085bd0940a7a29e1a2440f120f3473afeb3
Oscar He <oscarhe91@gmail.com>
2013-08-06 16:45:07 -0400

    Completed a simple fragments test.

--- Fragments/src/com/spoobrain/fragments/MainActivity.java
+++ Fragments/src/com/spoobrain/fragments/MainActivity.java
@@ -2,6 +2,8 @@ package com.spoobrain.fragments;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +12,18 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		FragmentManager manager = getFragmentManager();
+		
+		// To signal it is ready to do fragment stuff
+		FragmentTransaction transaction = manager.beginTransaction();
+		
+		MyFragment frag = new MyFragment();
+		
+		transaction.add(R.id.fragmentTextView, frag);
+		
+		transaction.commit();
+		
 	}
 
 	@Override
