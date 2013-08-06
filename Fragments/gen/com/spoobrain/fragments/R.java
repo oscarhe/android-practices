4fd95fc00ab278945ad0021e1fe3ee6714e66438
Oscar He <oscarhe91@gmail.com>
2013-08-06 16:26:47 -0400

    Added the layout.

--- Fragments/gen/com/spoobrain/fragments/R.java
+++ Fragments/gen/com/spoobrain/fragments/R.java
@@ -19,12 +19,14 @@ public final class R {
          */
         public static final int activity_horizontal_margin=0x7f040000;
         public static final int activity_vertical_margin=0x7f040001;
+        public static final int component_padding=0x7f040002;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080001;
+        public static final int fragmentTextView=0x7f080000;
     }
     public static final class layout {
         public static final int activity_fragment=0x7f030000;
